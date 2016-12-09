e90d1f9c9591e275c39a8cfad8fe1f8656a9c7c2
Alex Martin <martin.alex.t@gmail.com>
2016-12-09 01:46:11 -0500

    First working listener.

--- gab.go
+++ gab.go
@@ -2,9 +2,21 @@ package main
 
 import (
   "fmt"
+  "os"
 )
 
 func main() {
-  name := getName()
-  fmt.Println(name)
+  conn, err := slackInit(os.Getenv("SLACKBOT_TOKEN"))
+  if err != nil {
+    fmt.Println("Listen harder! Websocket connection failed.")
+    return
+  }
+
+  fmt.Println("Now you're cooking! Kill process to exit.")
+
+  for {
+    _, message, err := conn.ReadMessage()
+    if err != nil { return }
+    fmt.Println(string(message))
+  }
 }
--- slack-rtm.go
+++ slack-rtm.go
@@ -1,9 +1,49 @@
 package main
 
 import (
+  // Native packages
+  "encoding/json"
+  "fmt"
+  "io/ioutil"
+  "net/http"
+
+  // External packages
+  "github.com/gorilla/websocket"
 )
 
-func getName() (name string) {
-  name = "Lex"
+// *** STRUCTS ***
+type slackWebsocket struct {
+  Url string
+}
+
+// *** METHODS **
+// Get the Realtime Messaging API Websocket URL
+func slackGetWebsocket(apiToken string) (socketAddress string, err error) {
+  endpoint := fmt.Sprintf("https://slack.com/api/rtm.start?token=%s", apiToken)
+  response, err := http.Get(endpoint)
+  if err != nil { return }
+
+  body, err := ioutil.ReadAll(response.Body)
+  if err != nil { return }
+
+  var websocketData slackWebsocket
+  err = json.Unmarshal(body, &websocketData)
+
+  socketAddress = websocketData.Url
   return
 }
+
+
+// Kick off the websocket listener
+func slackInit(apiToken string) (conn *websocket.Conn, err error) {
+  websocketUrl, err := slackGetWebsocket(apiToken)
+  if err != nil { return }
+
+  // Get a live connection with no special headers
+  conn, _, err = websocket.DefaultDialer.Dial(websocketUrl, nil)
+
+  // It's cool if there's an error - it'll be in `err`.
+  return
+}
+
+
